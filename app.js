27478622f1682d48263164deb44b5acf483276a5
riceriley59 <64724036+riceriley59@users.noreply.github.com>
2022-07-31 16:03:45 -0700

    added super simple restful api for posts

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 //import and initialize express module
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv/config');
+const postsRoute = require('./routes/posts');
+
 const app = express();
 
-//ROUTES 
-app.get('/', (req, res) => {
-    res.send("We are on Home");
-});
+app.use(bodyParser.json());
+app.use('/posts', postsRoute);
+
+//connect to database
+mongoose.connect(
+    process.env.DB_connection, 
+    {useNewUrlParser: true},
+    ()=> console.log('Connected to DB')
+);
 
 //start listenting for server on port 3000
 app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Posts');
+
+//gets all posts
+router.get('/', async(req, res)=>{
+    try {
+        const Posts = await Post.find();
+        res.json(posts);
+    } catch (error) {
+        res.json({message:error});
+    }
+});
+
+//sumbits a post
+router.post('/', async(req, res)=>{
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });  
+
+    try{
+        const savedPost = await post.save()
+        res.json(savedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//get specific Post
+router.get('/:postId', async(req, res)=>{
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+//delete a post
+router.delete('/:postId', async(req, res)=>{
+    try{
+        const removedPost = await Post.remove({_id: req.params.postId});
+        res.json(removedPost);
+    } catch(err){
+        res.json({message: err });
+    }
+});
+
+//update a post
+router.patch('/:postId', async(req, res)=>{
+    try{
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId}, 
+            {$set: {title: req.body.title}}
+        );
+        
+        res.json(updatedPost);
+    } catch(err){
+        res.json({message: err });
+    }
+});
+
+module.exports = router;
